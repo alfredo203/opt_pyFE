c5dec5af849ea900648e1029677bfd22932e7647
Dayjingg <reydavid10910@gmail.com>
2025-05-20 14:58:03 -0600

    Update Empaquetado_bandas_bollinger_analiticos.py

--- src/opt_pyFE/Empaquetado_bandas_bollinger_analiticos.py
+++ src/opt_pyFE/Empaquetado_bandas_bollinger_analiticos.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr  1 14:03:09 2025
+Created on Tue May 13 14:19:16 2025
 
 @author: David
 """
 
 import yfinance as yf
 import matplotlib.pyplot as plt
-import pandas as pd 
 
 def bandas_bollinger(tickers, start_date, end_date, window=20):
-
     
     # pone las acciones en formato de lista para poder precesar mas de uno
     if isinstance(tickers, str):  
@@ -21,9 +19,6 @@ def bandas_bollinger(tickers, start_date, end_date, window=20):
 
     for ticker in tickers:
         data = yf.download(ticker, start=start_date, end=end_date)
-
-        if not isinstance(data, pd.DataFrame):
-            data = pd.DataFrame()
         
         # Calculo de la media movil y desviacion estandar
         data['MA20'] = data['Close'].rolling(window=window).mean()
@@ -40,9 +35,21 @@ def bandas_bollinger(tickers, start_date, end_date, window=20):
         # Grafico individual para cada ticker
         plt.figure(figsize=(10, 6))
         plt.plot(data['Close'], label='Precio de cierre', linewidth=1.5)
-        plt.plot(data['UpperBand'], linestyle='--', linewidth=1, label='Banda Superior')
-        plt.plot(data['MiddleBand'], linestyle='--', linewidth=1, label='Banda Media')
-        plt.plot(data['LowerBand'], linestyle='--', linewidth=1, label='Banda Inferior')
+        plt.plot(data['UpperBand'], 
+                 linestyle='--', 
+                 linewidth=1, 
+                 color = "red",
+                 label='Banda Superior')
+        plt.plot(data['MiddleBand'], 
+                 linestyle='--', 
+                 linewidth=1,
+                 color = "blue",
+                 label='Banda Media')
+        plt.plot(data['LowerBand'], 
+                 linestyle='--', 
+                 linewidth=1, 
+                 color = "red",
+                 label='Banda Inferior')
 
         plt.title(f'Bandas de Bollinger - {ticker}')
         plt.xlabel('Fecha')
@@ -58,5 +65,3 @@ start_date = '2024-01-01'
 end_date = '2024-06-01'
 
 bandas_bollinger(tickers, start_date, end_date)
-
-             # if de data frame si son datos hacerlo dataframe y si no es data frame convertirlo en data frame
